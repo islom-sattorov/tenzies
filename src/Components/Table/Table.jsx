7ce034260f6c9f0e77496b30fee20757a8c233ed
script <slmsttrv1@gmail.com>
2022-10-18 04:52:41 +0500

    add: function randomDice

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -3,7 +3,19 @@ import { useState } from "react";
 function Table() {
     const [numbers, setNumbers] = useState(() => ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]);
 
-    const renderedNumbers = numbers.map((item, idx) => <div className="number" key={idx}>{item}</div>)
+
+
+    function randomDice() {
+        let randomNumber = [];
+        for (let i = 0; i < 10; i++) {
+            randomNumber.push(Math.floor(Math.random() * 6) + 1)
+        }
+
+        return randomNumber
+    }
+
+    const renderedNumbers = randomDice().map((item, idx) => <div className="number" key={idx}>{item}</div>)
+
 
     return (
         <div className="numbers">
