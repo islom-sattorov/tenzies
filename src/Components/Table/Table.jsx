fbb4dabd564627296f5d021ba131d17a4ee511e1
script <slmsttrv1@gmail.com>
2022-10-18 04:40:49 +0500

    fix: css

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function Table() {
-    const [numbers, setNumbers] = useState(() => ["1", "2", "1", "4", "5", "3", "3", "5", "6", "1"]);
+    const [numbers, setNumbers] = useState(() => ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]);
 
     const renderedNumbers = numbers.map((item, idx) => <div className="number" key={idx}>{item}</div>)
 
